3569874ee42f4ea3bb6c056604557a4e7d8c67f5
Shoaib <shoaib713343@gmail.com>
2025-08-05 16:21:55 +0530

    add register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,55 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
+import ApiError from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+import {uploadOnCloudinary} from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-    return res.status(201).json({
-        message: "User registered successfully",
-    });
+    
+    const {fullName, email, username, password} = req.body;
+    console.log(email);
+
+    if([fullName, email, username, password].some(field => field?.trim() === "")) {
+        throw new ApiError(400, "All fields are required");
+    }
+
+    const existedUser = User.findOne(
+        { $or: [{ email }, { username }] },
+    )
+    
+    if (existedUser) {
+        throw new ApiError(409, "User with this email or username already exists");
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar is required");
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+    if (!avatar) {
+        throw new ApiError(500, "Failed to upload avatar");
+    }
+
+    const user = await User.create({
+        fullName,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        username: username.toLowerCase(),   
+        password,
+    })
+    const createdUser = await User.findById(user._id).select("-password -refreshToken");
+
+    if (!createdUser) {
+        throw new ApiError(500, "Failed to create user");
+    }
+
+    return res.status(201).json(new ApiResponse(201, createdUser, "User registered successfully"));
 })
 
 export { registerUser };
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -23,7 +23,7 @@ const userSchema = new Schema(
             type: String,
             required: true,
             trim: true,
-            index,
+            
         },
         avatar: {
             type: String,
